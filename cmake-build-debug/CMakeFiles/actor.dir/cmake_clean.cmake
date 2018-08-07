3561665eb4e686bcc22c291f6d8c0417fbfecfef
Thomas Deniffel <tdeniffel@googlemail.com>
2018-08-07 20:26:29 +0200

    Specified goals

---
+++ cmake-build-debug/CMakeFiles/actor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/actor.dir/main.cpp.o"
+  "actor.pdb"
+  "actor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/actor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
